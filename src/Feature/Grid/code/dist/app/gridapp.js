ed364e7ce37837b9837cd1b53e5f7524408a1eab
Andrew Komissarzhevskiy <andrew.komissarzhevskiy@sagittarius-marketing.com>
2021-03-06 16:56:00 +0200

    Fixed request url

--- src/Feature/Grid/code/dist/app/gridapp.js
+++ src/Feature/Grid/code/dist/app/gridapp.js
@@ -2,8 +2,8 @@
     $("div.gridapp").Grid({
         columns: ['Name', 'Age', 'Email'],
         server: {
-            url: 'https://hackathon101sc.dev.local/grid/gridapplication/data?id={11111111-1111-1111-1111-111111111111}&database=master',
-            then: data => data.Data.map(item => [item[0], item[1], item[2]])
+            url: '/grid/gridapplication/data?id=' + itemId + '&database='+ database,
+            then: data => data.Data.map(item => [item[0], item[1], item[2], item[3], item[4], item[5]])
         }
     });
 });
